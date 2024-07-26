f3325275d5a74c51f33f7f969dbff8a00ec25fb2
Ailer-h <henriqueailer2@gmail.com>
2024-07-26 16:23:35 -0300

    Finished the landing page design

--- php/index.php
+++ php/index.php
@@ -8,11 +8,56 @@
 </head>
 <body>
     <div class="navbar">
-        <h1>Six Jars</h1>
+        <h1>Wealthier</h1>
         <div class="menu">
             <button class="secondary">Sign In</button>
             <button class="primary">Log In</button>
         </div>
     </div>
+    <div class="banner">
+        <div class="title">
+            <h1>Wealthier</h1>
+            <h2>Control your money, create your wealth.</h2>
+        </div>
+    </div>
+
+    <div class="container">
+        <div class="values">
+            <div class="card">
+                <div class="icon">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 14 14"><g fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"><path d="M.5.5v13h13"/><path d="M3.5 6.5L6 9l4-6l3.5 2.5"/></g></svg>
+                </div>
+                <h1>Track your spendings</h1>
+            </div>
+
+            <div class="card">
+                <div class="icon">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="1.13em" height="1em" viewBox="0 0 576 512"><path fill="currentColor" d="M560 224h-29.5c-8.8-20-21.6-37.7-37.4-52.5L512 96h-32c-29.4 0-55.4 13.5-73 34.3c-7.6-1.1-15.1-2.3-23-2.3H256c-77.4 0-141.9 55-156.8 128H56c-14.8 0-26.5-13.5-23.5-28.8C34.7 215.8 45.4 208 57 208h1c3.3 0 6-2.7 6-6v-20c0-3.3-2.7-6-6-6c-28.5 0-53.9 20.4-57.5 48.6C-3.9 258.8 22.7 288 56 288h40c0 52.2 25.4 98.1 64 127.3V496c0 8.8 7.2 16 16 16h64c8.8 0 16-7.2 16-16v-48h128v48c0 8.8 7.2 16 16 16h64c8.8 0 16-7.2 16-16v-80.7c11.8-8.9 22.3-19.4 31.3-31.3H560c8.8 0 16-7.2 16-16V240c0-8.8-7.2-16-16-16m-128 64c-8.8 0-16-7.2-16-16s7.2-16 16-16s16 7.2 16 16s-7.2 16-16 16M256 96h128c5.4 0 10.7.4 15.9.8c0-.3.1-.5.1-.8c0-53-43-96-96-96s-96 43-96 96c0 2.1.5 4.1.6 6.2c15.2-3.9 31-6.2 47.4-6.2"/></svg>
+                </div>
+                <h1>Save your money</h1>
+            </div>
+
+            <div class="card">
+                <div class="icon">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 14 14"><g fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"><path d="M4.4 7v2.31m1.156-1.154H3.245m-1.989-.963l-.412 3.71a2.283 2.283 0 0 0 4.311 1.272l.36-.718h2.97l.36.718a2.283 2.283 0 0 0 4.31-1.273l-.411-3.71a3 3 0 0 0-2.982-2.668H4.238a3 3 0 0 0-2.982 2.669"/><path d="M7 4.524v-.98a1 1 0 0 1 1-1h1.466a1 1 0 0 0 1-1V.561m0 6.934a.248.248 0 0 1 0-.495m0 .495a.248.248 0 1 0 0-.495M8.733 8.733a.248.248 0 0 1 .495 0m-.495 0a.248.248 0 0 0 .495 0"/></g></svg>
+                </div>
+                <h1>Control your life</h1>
+            </div>
+        </div>
+    </div>
+
+    <hr>
+
+    <div class="container">
+        <div class="text">
+            <h1>With wealthier, it's simple!</h1>
+            <p>Using the Six Jars method, we can help you to easily keep track of your spendings and develop healthier money habits.</p>
+        </div>
+    </div>
+
+    <div class="footer">
+        <p>Henrique Ailer 2024 - Todos os direitos reservados</p>
+    </div>
+
 </body>
 </html>
