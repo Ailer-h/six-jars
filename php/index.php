a5881f935b3b22fa605b2040deebd4123254f636
Ailer-h <henriqueailer2@gmail.com>
2024-07-26 12:05:16 -0300

    Initial Commit. Started working on the navbar

---
+++ php/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../css/index.css">
+    <title>Six Jars</title>
+</head>
+<body>
+    <div class="navbar">
+        <h1>Six Jars</h1>
+        <div class="menu">
+            <button class="secondary">Sign In</button>
+            <button class="primary">Log In</button>
+        </div>
+    </div>
+</body>
+</html>
